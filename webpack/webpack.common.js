3e354729104c61eeb37a1f8129650b39a350c2d6
shaibachar <shaibachar@gmail.com>
2025-03-22 21:44:24 +0200

    add he

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -135,7 +135,7 @@ module.exports = async options => {
           output: {
             groupBy: [
               { pattern: './src/main/webapp/i18n/en/*.json', fileName: './i18n/en.json' },
-              { pattern: './src/main/webapp/i18n/de/*.json', fileName: './i18n/de.json' },
+              { pattern: './src/main/webapp/i18n/he/*.json', fileName: './i18n/he.json' },
               // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
             ],
           },
